91ffae06222dcc0cb75db0fd165191897c6c97de
aes89 <adrienne.e.smith@outlook.com>
2020-08-09 13:44:47 +1000

    Styling to New/Edit/Individual Listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,7 @@ class Listing < ApplicationRecord
     
     enum material: {unknown: 0, plastic: 1, cement: 2, teracotta: 3, ceramic: 4, metal: 5, wicker: 6, wire: 7}
     enum shape: {other: 0, round: 1, square: 2}
-    enum drainage: {Drainage: 0, "No Drainage": 2}
+    enum drainage: {"Has Drainage": 0, "No Drainage": 2}
     enum saucer: {"Saucer Included": 0, "No Saucer Included": 1}
 
     validates :title, :description, :material, :height, :width, :shape, :drainage, :saucer, :price, presence: true
